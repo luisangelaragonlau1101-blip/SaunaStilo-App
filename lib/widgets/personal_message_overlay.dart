2bb4fa6eca2761b881c4ea67b00d553c0152af73
luisangelaragonlau1101-blip <luisangel.aragon.lau1101@gmail.com>
2026-09-05 16:04:37 -0600

    fix: disambiguate Firestore server reads from audio source types

--- lib/widgets/personal_message_overlay.dart
+++ lib/widgets/personal_message_overlay.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart' hide Source;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
