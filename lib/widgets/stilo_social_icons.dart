57f3d0b1344f65cc780eb882e3b447f2cc3bb9ac
luisangelaragonlau1101-blip <luisangel.aragon.lau1101@gmail.com>
2026-09-05 15:47:49 -0600

    ui: add Sauna Stilo outline social icons without copying a third-party brand

---
+++ lib/widgets/stilo_social_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Company-neutral outline vocabulary shared by social and evidence controls.
+class StiloSocialIcons {
+  static const IconData community = Icons.auto_awesome_mosaic_outlined;
+  static const IconData evidence = Icons.add_photo_alternate_outlined;
+  static const IconData comments = Icons.mode_comment_outlined;
+  static const IconData chat = Icons.forum_outlined;
+  static const IconData projectGroup = Icons.workspaces_outline;
+  static const IconData voice = Icons.multitrack_audio_rounded;
+  static const IconData profile = Icons.person_outline_rounded;
+}
